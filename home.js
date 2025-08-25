a936e9560684108835392de0b071387768b1c4d3
hasanali3597 <hasanali3597@gmail.com>
2025-08-26 01:57:52 +0600

    amonet

---
+++ home.js
@@ -0,0 +1,23 @@
+// add moneytotalAvailableBalance
+
+
+document.getElementById('addMoneyBtn')
+.addEventListener('click', function (event){
+    event.preventDefault()
+// console.log('jadhc')
+const bank = document.getElementById('bank').value;
+const bankNumber =document.getElementById('bank-number').value; 
+const addAmount =parseInt(document.getElementById('add-amount').value);
+const accountpin = document.getElementById('account-pin').value;
+
+
+const availableBalance = parseInt(document.getElementById('available-balance').innerText)
+// console.log(availableBalance)
+
+const totalAvailableBalance = availableBalance + addAmount;
+
+document.getElementById('available-balance').innerText = totalAvailableBalance
+
+})
+
+
