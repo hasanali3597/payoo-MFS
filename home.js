eb5a7d1567e3bb446d0230bb4d55c348d6ffbc2a
hasanali3597 <hasanali3597@gmail.com>
2025-08-26 11:57:31 +0600

    javascript completd

--- home.js
+++ home.js
@@ -1,23 +1,32 @@
 // add moneytotalAvailableBalance
 
+const validpin = 1234;
+
 
 document.getElementById('addMoneyBtn')
-.addEventListener('click', function (event){
-    event.preventDefault()
-// console.log('jadhc')
-const bank = document.getElementById('bank').value;
-const bankNumber =document.getElementById('bank-number').value; 
-const addAmount =parseInt(document.getElementById('add-amount').value);
-const accountpin = document.getElementById('account-pin').value;
+    .addEventListener('click', function (event) {
+        event.preventDefault()
+        // console.log('jadhc')
+        const bank = document.getElementById('bank').value;
+        const bankNumber = document.getElementById('bank-number').value;
+        const addAmount = parseInt(document.getElementById('add-amount').value);
+        const accountpin = parseInt(document.getElementById('account-pin').value);
 
 
-const availableBalance = parseInt(document.getElementById('available-balance').innerText)
-// console.log(availableBalance)
 
-const totalAvailableBalance = availableBalance + addAmount;
+        const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+        if (bankNumber.length < 11) {
+            alert("please provide valid number").repeat;
+        }
+        if (validpin != accountpin) {
+            alert('please provide valid pin number')
+        }
 
-document.getElementById('available-balance').innerText = totalAvailableBalance
 
-})
+        const totalAvailableBalance = availableBalance + addAmount;
+
+        document.getElementById('available-balance').innerText = totalAvailableBalance
+
+    })
 
 
